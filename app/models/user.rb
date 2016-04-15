44519794a436876eb8b1d133949e894a15266580
Chunyu Ou(欧春雨) <ouchunyu@yahoo.com>
2016-04-14 20:08:43 -0700

    Finish models validations and data tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   has_many :active_relationships, class_name: "Relationship",
                                   foreign_key: "followed_id",
@@ -9,8 +9,16 @@ class User < ActiveRecord::Base
                                   foreign_key: "follower_id",
                                   dependent: :destroy
 
-  has_many :followings, through: :active_relationships, source: :followed
+  has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save {self.email.downcase!}
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length:     { maximum: 150 },
+                                    format:     { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
+
 end
-# has_many :followers, through: :relationships, source: :
+
