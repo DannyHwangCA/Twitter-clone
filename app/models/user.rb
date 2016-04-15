7873ed77564cdcff3a6e320404da4d2f831e3d95
Peter Kang <email": null>
2016-04-15 08:45:41 -0700

    WIP authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ActiveRecord::Base
+
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
+  before_save {self.email.downcase!}
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length:     { maximum: 150 },
+                                    format:     { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
+
   has_many :posts, dependent: :destroy
 
   has_many :active_relationships, class_name: "Relationship",
@@ -12,7 +22,7 @@ class User < ActiveRecord::Base
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
-<<<<<<< HEAD
+
   include BCrypt
 
   def password
@@ -29,21 +39,7 @@ class User < ActiveRecord::Base
     self.password == password
   end
 
-end
 
 
 
 end
-
-=======
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-
-  before_save {self.email.downcase!}
-  validates :username, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length:     { maximum: 150 },
-                                    format:     { with: VALID_EMAIL_REGEX },
-                                    uniqueness: { case_sensitive: false }
-
-end
-
->>>>>>> 7f41032bff8324d15bcb7f56e5264b37f58bb041
