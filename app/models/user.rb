c2870de8171977f37fe5173dece7f8e82d74ff84
Shyh Hwang <email": null>
2016-04-14 20:26:22 -0700

    Add Login files, login.erb, session

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,25 @@ class User < ActiveRecord::Base
   has_many :followings, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash) if password_hash.present?
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
+
+  def authenticate(password)
+    self.password == password
+  end
+
 end
-# has_many :followers, through: :relationships, source: :
+
+
+
+end
+
