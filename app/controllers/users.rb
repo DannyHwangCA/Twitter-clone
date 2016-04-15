21102e7394810f0487baca263edb0e95efaeec3c
Shyh Hwang <dannyhwangca@gmail.com>
2016-04-15 10:32:06 -0700

    Update users.rb

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,15 +1,20 @@
-get '/signup' do
-  erb :'users/new_user'
-end
-
-post '/signup' do
-  redirect '/session'
-end
-
-get '/profile' do
-
-end
-
-patch '/profile' do
-
-end
++get '/users/signup' do
+    erb :'users/new_user'
+  end
+  
+  +post '/users/signup' do
+ +  user = User.create(username: params[:username], email: params[:email], password: params[:password])
+ +  session[:success] = user
+ +  redirect '/loggedin_index'
+  end
+  
+  +get '/users/:id/edit' do
+ +  @user = User.find(params[:id])
+ +  erb :edit_user
+  end
+  
+  +patch '/users/:id' do
+ +  user = User.find(params[:id])
+ +  user.update_attributes(username: params[:username], email: params[:email], password: params[:password])
+ +  redirect '/users/show' #profile
+  end
