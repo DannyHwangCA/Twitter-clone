38932fce6d2de37b29eeb7c19e1ff936fbfee17f
Kevin Niu <email": null>
2016-04-14 21:19:30 -0700

    Add edit and signup for users

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,15 +1,20 @@
-get '/signup' do
+get '/users/signup' do
   erb :'users/new_user'
 end
 
-post '/signup' do
-  redirect '/session'
+post '/users/signup' do
+  user = User.create(username: params[:username], email: params[:email], password: params[:password])
+  session[:success] = user
+  redirect '/loggedin_index'
 end
 
-get '/profile' do
-
+get '/users/:id/edit' do
+  @user = User.find(params[:id])
+  erb :edit_user
 end
 
-patch '/profile' do
-
+patch '/users/:id' do
+  user = User.find(params[:id])
+  user.update_attributes(username: params[:username], email: params[:email], password: params[:password])
+  redirect '/users/show' #profile
 end
