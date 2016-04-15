fbc35210485ee5570c8aea4492bd9f6a88a59a74
Peter Kang <email": null>
2016-04-15 11:55:59 -0700

    fix post code

--- app/controllers/post.rb
+++ app/controllers/post.rb
@@ -1,18 +1,22 @@
 get '/users/:user_id/posts' do
+  @user = User.find(params[:user_id])
   @posts = Post.where(user_id: params[:user_id])
   erb :loggedin_index
 end
 
 post '/users/:user_id/posts' do
-  Post.create(content: params[:post], user_id: params[:user_id])
-  redirect '/users/:user_id/posts'
+  @posts = Post.create(content: params[:tweet], user_id: params[:user_id])
+  redirect "/users/#{@posts.user_id}/posts"
 end
 
 get '/users/:user_id/posts/:post_id' do
   @post = Post.find_by(id: params[:post_id])
-  erb :show_post
+  erb :'posts/show_post'
 end
 
 patch '/users/:user_id/posts/:post_id' do
-  @post = Post.update_attributes(content: params[:content])
+  @post = Post.find(params[:post_id])
+  @post.content = params[:content]
+  @post.save
+  redirect "/users/#{@post.user_id}/posts/#{@post.id}"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
-
-
-
+User.create(username: "Peterrkang", email: "peterrkang@gmail.com", password_hash: "peter")
+Post.create(content: "dont sweat it", user_id: 1)
+User.create(username: "Kevin", email: "Kevin@aol.com", password_hash: "kevin")
+Post.create(content: "hehhehehehe", user_id: 1)
