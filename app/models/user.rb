ae170b65e67fffd51e783087d5a488574ec64e12
Chunyu Ou(欧春雨) <ouchunyu@yahoo.com>
2016-04-15 17:46:21 -0700

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
   has_many :active_relationships, class_name: "Relationship",
-                                  foreign_key: "followed_id",
+                                  foreign_key: "follower_id",
                                   dependent: :destroy
 
   has_many :passive_relationships, class_name: "Relationship",
-                                  foreign_key: "follower_id",
+                                  foreign_key: "followed_id",
                                   dependent: :destroy
 
   has_many :following, through: :active_relationships, source: :followed
